053c57e957b374061ab990b9b11268d1792e09d5
mk-hasan <mkhasan6891@gmail.com>
2022-04-15 15:18:53 +0600

    social login component create in login page folder

---
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SocialLogin = () => {
+    return (
+        <div className=' d-flex align-items-center'>
+            <div style={{ height: '2px' }} className='bg-primary w-50'></div>
+            <p className=' m-2 px-2'>OR</p>
+            <div style={{ height: '2px' }} className=' bg-primary w-50'></div>
+        </div>
+    );
+};
+
+export default SocialLogin;
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
     return (
         <div>
             <h2>Service Detail : {serviceId} </h2>
+            <Link to='/checkout'><button className='btn btn-primary'>Proceed Checkout</button></Link>
         </div>
     );
 };
